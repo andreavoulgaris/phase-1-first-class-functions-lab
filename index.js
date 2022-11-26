01bd0c5c06ed3341a1c9c1569aa541ba71b24c92
Andrea Voulgaris <abv@umich.edu>
2022-11-25 22:04:05 -0500

    commit tree 6ad15980603a04cfcc7b557b846666bc07c84965

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(anArray) {
+    return ['Antonia', 'Nuru']
+}
+
+const returnLastTwoDrivers = function(anArray) {
+    return ['Amari', 'Mo']
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const fareMultiplier = function(integer) {
+    return integer * 5
+}
+function createFareMultiplier(integer) {
+    return fareMultiplier
+}
